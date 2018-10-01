bf8652d88a4412cbf7c973a5c4a273eeefd5f936
thiagomartendal <thiagomartendal@gmail.com>
2018-10-01 15:01:22 -0200

    Add files via upload

--- Trabalho1/Trabalho1/Automato.cs
+++ Trabalho1/Trabalho1/Automato.cs
@@ -1,142 +1,59 @@
-using System;
+ï»¿using System;
 using System.Collections.Generic;
 
-namespace Trabalho1
-{
-    public class Automato
-    {
-        private List<string[]> automato;
+namespace Trabalho1 {
+	public class Automato {
+		private string estadoInicial;
+		private HashSet<string> estadosFinais;
+		private HashSet<Transicao> transicoes;
 
-        private string procuraEstadoInicial()
-        {
-            string estadoInicial = "";
-            for (int i = 0; i < automato.Count; i++)
-            {
-                string[] particao = automato[i];
-                string estado = particao[0];
-                if (estado.Contains("+"))
-                {
-                    estadoInicial = estado;
-                    break;
-                }
-                else
-                {
-                    if (i == automato.Count - 1 && !estado.Contains("+"))
-                    {
-                        Console.WriteLine("não tem estado inicial");
-                    }
-                }
-            }
-            return estadoInicial;
-        }
+		private struct Transicao {
+			public string estado1;
+			public string simbolo;
+			public string estado2;
+		}
 
-        private List<string> procuraEstadoFinal()
-        {
-            List<string> estadosFinais = new List<string>();
-            for (int i = 0; i < automato.Count; i++)
-            {
-                string[] particao = automato[i];
-                string estado1 = particao[0];
-                string estado2 = particao[2];
-                if (estado1.Contains("*"))
-                {
-                    if (!estadosFinais.Contains(estado1))
-                    {
-                        estadosFinais.Add(estado1);
-                    }
-                }
-                else if (estado2.Contains("*"))
-                {
-                    if (!estadosFinais.Contains(estado2))
-                    {
-                        estadosFinais.Add(estado2);
-                    }
-                }
-            }
-            return estadosFinais;
-        }
+		public Automato () {
+			
+		}
 
-        private bool eNaoDeterministico()
-        {
-            for (int i = 0; i < automato.Count; i++)
-            {
-                string[] particao = automato[i];
-                if (particao[1] == "&")
-                {
-                    return true;
-                }
-                else
-                {
-                    for (int j = 0; j < i; j++)
-                    {
-                        string[] particao2 = automato[j];
-                        if (particao[0] == particao2[0] && particao[1] == particao2[1])
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
-        }
+		public void setEstadoInicial(string estadoInicial) {
+			this.estadoInicial = estadoInicial;
+			procuraEstadosFinais ();
+		}
 
-        private List<string[]> trasicoesNaoDeterministicas()
-        {
-            List<string[]> transicoes = new List<string[]>();
-            for (int i = 0; i < automato.Count; i++)
-            {
-                string[] particao = automato[i];
-                if (particao[1] == "&")
-                {
-                    string[] transicao1 = { particao[0], particao[1], particao[2] };
-                    transicoes.Add(transicao1);
-                }
-                for (int j = 0; j < i; j++)
-                {
-                    string[] particao2 = automato[j];
-                    if (particao[0] == particao2[0] && particao[1] == particao2[1])
-                    {
-                        string[] transicao2 = { particao[0], particao[1], particao[2] };
-                        string[] transicao3 = { particao2[0], particao2[1], particao2[2] };
-                        transicoes.Add(transicao2);
-                        transicoes.Add(transicao3);
-                    }
-                }
-            }
-            return transicoes;
-        }
+		private void procuraEstadosFinais() {
+			if (estadoInicial.Contains("*")) {
+				this.estadosFinais.Add (estadoInicial);
+			}
+			for (int i = 0; i < transicoes.Count; i++) {
+				Transicao T = transicoes [i];
+				if (T.estado1.Contains("*")) {
+					this.estadosFinais.Add (T.estado1);
+				}
+				if (T.estado2.Contains("*")) {
+					this.estadosFinais.Add (T.estado2);
+				}
+			}
+		}
 
-        public Automato(List<string[]> atm)
-        {
-            automato = atm;
-            Console.WriteLine("Estado inicial: " + procuraEstadoInicial());
-            for (int i = 0; i < procuraEstadoFinal().Count; i++)
-            {
-                Console.WriteLine("Estado Final: " + procuraEstadoFinal()[i]);
-            }
-            eNaoDeterministico();
-            for (int i = 0; i < trasicoesNaoDeterministicas().Count; i++)
-            {
-                string[] particao = trasicoesNaoDeterministicas()[i];
-                Console.WriteLine("Transições não-determinísticas: " + particao[0] + " " + particao[1] + " " + particao[2]);
-            }
-            for (int i = 0; i < trasicoesNaoDeterministicas().Count; i++)
-            {
-                string[] particao = trasicoesNaoDeterministicas()[i];
-                for (int j = 0; j < i; j++)
-                {
-                    string[] particao2 = trasicoesNaoDeterministicas()[j];
-                    if (particao[0] == particao2[0] && particao[1] == particao2[1])
-                    {
-                        Console.WriteLine("União de Estados: " + particao[2] + particao2[2]);
-                    }
-                }
-            }
-        }
+		public void setTransicao(string estado1, string simbolo, string estado2) {
+			Transicao T = new Transicao ();
+			T.estado1 = estado1;
+			T.simbolo = simbolo;
+			T.estado2 = estado2;
+		}
 
-        public List<string[]> getAutomato()
-        {
-            return automato;
-        }
-    }
+		public string getEstadoInicial() {
+			return estadoInicial;
+		}
+	
+		public HashSet<string> getEstadosFinais() {
+			return estadosFinais;
+		}
+
+		public HashSet<Transicao> getTransicoes() {
+			return transicoes;
+		}
+	}
 }
