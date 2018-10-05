4b9dbca05b3b0c80ce5df1707eff0fa221a100b7
thiagomartendal <thiagomartendal@gmail.com>
2018-10-04 22:02:24 -0200

    Add files via upload

--- Trabalho1/Trabalho1/MainWindow.cs
+++ Trabalho1/Trabalho1/MainWindow.cs
@@ -3,76 +3,76 @@ using Gtk;
 using Trabalho1;
 using System.Collections.Generic;
 
-public partial class MainWindow : Gtk.Window
-{
-    private List<string[]> automato;
-    private NovoAutomato novo;
+public partial class MainWindow: Gtk.Window {
+	private NovoAutomato novo;
 
-    public MainWindow() : base(Gtk.WindowType.Toplevel)
-    {
-        Build();
-    }
+	public MainWindow () : base (Gtk.WindowType.Toplevel) {
+		Build ();
+	}
 
-    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
-    {
-        Application.Quit();
-        a.RetVal = true;
-    }
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a) {
+		Application.Quit ();
+		a.RetVal = true;
+	}
 
-    protected void inserirAutomato(object sender, EventArgs e)
-    {
-        novo = new NovoAutomato(this);
-        Automato novoAutomato = novo.getAutomato();
-        //automato = novoAutomato.getAutomato();
-    }
+	protected void inserirAutomato (object sender, EventArgs e) {
+		novo = new NovoAutomato (this);
+	}
 
-    public void atualizarAutomato()
-    {
-        Table tabela = new Table((uint)automato.Count, 3, false);
-        tabela.BorderWidth = 0;
-        scrolledwindow1.Add(tabela);
-        Label lbl = new Label("Estado 1");
-        Label lbl2 = new Label("Transição");
-        Label lbl3 = new Label("Estado 2");
-        tabela.Attach(lbl, 0, 1, 0, 1);
-        tabela.Attach(lbl2, 1, 2, 0, 1);
-        tabela.Attach(lbl3, 2, 3, 0, 1);
-        lbl.Show();
-        lbl2.Show();
-        lbl3.Show();
-        uint inc = 2;
-        for (int i = 0; i < automato.Count; i++)
-        {
-            string[] p = automato[i];
-            Label lbl4 = new Gtk.Label(p[0]);
-            Label lbl5 = new Gtk.Label(p[1]);
-            Label lbl6 = new Gtk.Label(p[2]);
-            tabela.Attach(lbl4, 0, 1, (uint)i + 1, inc);
-            tabela.Attach(lbl5, 1, 2, (uint)i + 1, inc);
-            tabela.Attach(lbl6, 2, 3, (uint)i + 1, inc);
-            lbl4.Show();
-            lbl5.Show();
-            lbl6.Show();
-            inc++;
-        }
-        tabela.Show();
-    }
+	public void atualizarAutomato(string[] vetorEstados, string[] vetorSimbolos) {
+		Table tabela = new Table((uint)vetorEstados.Length, (uint)vetorSimbolos.Length, false);
+		tabela.BorderWidth = 0;
+		scrolledwindow1.Add(tabela);
+		uint inc1 = 2;
+		for (int i = 0; i < vetorSimbolos.Length; i++) {
+			Label lbl2 = new Label(vetorSimbolos[i]);
+			if (vetorSimbolos.Length == 1) {
+				tabela.Attach(lbl2, 1, 3, 0, 1);
+			} else {
+				tabela.Attach(lbl2, (uint)i, inc1+1, 0, 1);
+			}
+			lbl2.Show();
+			inc1++;
+		}
+		uint inc2 = 2;
+		for (int i = 0; i < vetorEstados.Length; i++) {
+			Label lbl3 = new Label(vetorEstados[i]);
+			if (vetorEstados.Length == 1) {
+				tabela.Attach (lbl3, 0, 2, 1, 3);
+			} else {
+				tabela.Attach (lbl3, 0, 1, (uint)i, inc2 + 1);
+			}
+			lbl3.Show();
+			inc2++;
+		}
+		for (int i = 0; i < vetorSimbolos.Length; i++) {
+			for (int j = 0; j < vetorEstados.Length; j++) {
+				Entry txt = new Entry();
+				txt.WidthChars = 5;
+				if (vetorSimbolos.Length == 1 && vetorEstados.Length == 1) {
+					tabela.Attach(txt, 1, (uint)i+3, 1, (uint)j+3, AttachOptions.Expand, AttachOptions.Fill, 0, 0);
+				} else if (vetorSimbolos.Length == 1) {
+					tabela.Attach(txt, 1, (uint)i+3, (uint)j, (uint)j+3, AttachOptions.Expand, AttachOptions.Fill, 0, 0);
+				} else if (vetorEstados.Length == 1) {
+					tabela.Attach(txt, (uint)i, (uint)i+3, 1, (uint)j+3, AttachOptions.Expand, AttachOptions.Fill, 0, 0);
+				} else {
+					tabela.Attach(txt, (uint)i, (uint)i+3, (uint)j, (uint)j+3, AttachOptions.Expand, AttachOptions.Fill, 0, 0);
+				}
+				txt.Show();
+			}
+		}
+		tabela.Show();
+	}
 
-    protected void OnAutomatoAction1Activated(object sender, EventArgs e)
-    {
+	protected void OnAutomatoAction1Activated (object sender, EventArgs e) {
         Crud crud = new Crud("Automato");
-        crud.Store<List<string[]>>(automato);
-    }
+	}
 
-    protected void OnAutomatoActionActivated(object sender, EventArgs e)
-    {
-        Crud crud = new Crud("Automato");
-        crud.Load<List<string[]>>(ref automato);
-        atualizarAutomato();
-    }
+	protected void OnAutomatoActionActivated (object sender, EventArgs e) {
+		Crud crud = new Crud("Automato");
+	}
 
-    protected void OnDeterminizarAutmatoActionActivated(object sender, EventArgs e)
-    {
-        //Automato atm = new Automato(automato);
-    }
+	protected void OnDeterminizarAutmatoActionActivated (object sender, EventArgs e) {
+		
+	}
 }
