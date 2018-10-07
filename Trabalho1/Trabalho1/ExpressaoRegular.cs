e45d037fe4c370f721b7362b851592a6873bcd63
LENOVO <LENOVO@ACER-PC>
2018-10-07 03:58:36 -0300

    Arvore Sintatica

--- Trabalho1/Trabalho1/ExpressaoRegular.cs
+++ Trabalho1/Trabalho1/ExpressaoRegular.cs
@@ -15,14 +15,47 @@ namespace Trabalho1
         {
             arvore = new ArvoreSintatica();
         }
-        public Automato transformERtoAFD(string text)
+        public Automato transformaERemAFD(string text)
         {
+            Console.WriteLine("start");
             Automato a = new Automato(1);
             regex = text;
-            createTree();
-            readTree();
+            createTree(); //Monta Arvore Sintatica
+            Console.WriteLine("1");
+            readTree(); //Marca Indicadores Folha e cria Pilha com Nodos Internos
+            Console.WriteLine("2");
+            arvore.copyStack(); //Duplica Pilha para os metodos seguintes
+            arvore.calculaPrimeiraEUltimaPosicao(); //Preenche pri_pos, ult_pos e anulavel - esvazia pilha 1
+            Console.WriteLine("3");
+            arvore.calculaTabelaPosicaoSeguinte(); // usa segunda pilha pra calcular posicao_seguinte
+            Console.WriteLine("4");
+            showPosSeguinte();
+            Console.WriteLine("5");
             return a;
         }
+
+        private void showPosSeguinte()
+        {
+            Console.WriteLine("enter");
+            foreach (DictionaryEntry element in arvore.posicao_seguinte)
+            {
+                Console.WriteLine("cjeck");
+                //obtém os valores da HashTable usando Key e Value
+                int i = (int)element.Key;
+                //HashSet<int> hash = (HashSet<int>)element.Value;
+                string pos="";
+                foreach (var v in (HashSet<int>)element.Value)
+                {
+                    pos = pos +","+ v.ToString();
+                }
+                //exibe os valores da HashTable
+                Console.WriteLine("i: {0}, pos: {1}", i, pos);
+            }
+
+        }
+
+ 
+
         public void createTree()
         {
             arvore.parseRegex(arvore.initialNodo(regex));
@@ -37,7 +70,9 @@ namespace Trabalho1
             public int qtNodo = 0;
             public String result;
             private int indicador;
-            public Stack<Nodo> NodosInternos;
+            public Stack<Nodo> NodosInternos = new Stack<Nodo>();
+            public Stack<Nodo> NodosPosicaoSeguinte = new Stack<Nodo>();
+            public Hashtable posicao_seguinte = new Hashtable();
 
             public class Nodo
             {
@@ -47,7 +82,7 @@ namespace Trabalho1
                 public string valor = "";
                 public HashSet<int> primeira_posicao = new HashSet<int>();
                 public HashSet<int> ultima_posicao = new HashSet<int>();
-                public HashSet<int> posicao_seguinte = new HashSet<int>();
+                internal bool anulavel;
             }
 
             public bool isLeaf(Nodo nodo)
@@ -145,9 +180,13 @@ namespace Trabalho1
                 if (isLeaf(nodo))
                 {
                     result = result + "{"+indicador+"}"+nodo.valor;
-                    indicador++;
+                    if (nodo.valor == "&")
+                        nodo.anulavel = true;
+                    else
+                        nodo.anulavel = false;
                     nodo.primeira_posicao.Add(indicador);
                     nodo.ultima_posicao.Add(indicador);
+                    indicador++;
                     return;
                 }
                 NodosInternos.Push(nodo);
@@ -163,11 +202,126 @@ namespace Trabalho1
                 readNodo(raiz);
                 return result;
             }
+            public void copyStack()
+            {
+                NodosPosicaoSeguinte = new Stack<Nodo>(NodosInternos.Reverse());
+            }
             public Nodo initialNodo(String regex)
             {
                 Nodo nodo = new Nodo();
                 nodo.valor = regex;
                 return nodo;
+            }
+
+            internal void calculaPrimeiraEUltimaPosicao()
+            {
+                Nodo auxNodo = new Nodo();
+                while(NodosInternos.Count > 0)
+                {
+                    auxNodo = NodosInternos.Pop();
+                    calculaAnulavel(auxNodo);
+                    calculaPrimeiraPos(auxNodo);
+                    calculaUltimaPos(auxNodo);
+                }
+            }
+            public void calculaAnulavel(Nodo nodo)
+            {
+                switch (nodo.valor)
+                {
+                    case "*":
+                        nodo.anulavel = true;
+                        break;
+                    case "|":
+                        nodo.anulavel = nodo.nodoEsquerda.anulavel || nodo.nodoDireita.anulavel;
+                        break;
+                    case ".":
+                        nodo.anulavel = nodo.nodoEsquerda.anulavel && nodo.nodoDireita.anulavel;
+                        break;
+                }
+
+            }
+            public void calculaPrimeiraPos(Nodo nodo)
+            {
+                switch (nodo.valor)
+                {
+                    case "*":
+                        nodo.primeira_posicao = nodo.nodoEsquerda.primeira_posicao;
+                        break;
+                    case "|":
+                        nodo.primeira_posicao = nodo.nodoEsquerda.primeira_posicao;
+                        nodo.primeira_posicao.UnionWith(nodo.nodoDireita.primeira_posicao);
+                        break;
+                    case ".":
+                        nodo.primeira_posicao = nodo.nodoEsquerda.primeira_posicao;
+                        if (nodo.nodoEsquerda.anulavel)
+                            nodo.primeira_posicao.UnionWith(nodo.nodoDireita.primeira_posicao);
+                        break;
+                }
+            }
+            public void calculaUltimaPos(Nodo nodo)
+            {
+                switch (nodo.valor)
+                {
+                    case "*":
+                        nodo.ultima_posicao = nodo.nodoEsquerda.ultima_posicao;
+                        break;
+                    case "|":
+                        nodo.ultima_posicao = nodo.nodoEsquerda.ultima_posicao;
+                        nodo.ultima_posicao.UnionWith(nodo.nodoDireita.ultima_posicao);
+                        break;
+                    case ".":
+                        nodo.ultima_posicao = nodo.nodoDireita.ultima_posicao;
+                        if (nodo.nodoDireita.anulavel)
+                            nodo.ultima_posicao.UnionWith(nodo.nodoEsquerda.ultima_posicao);
+                        break;
+                }
+            }
+
+            internal void calculaTabelaPosicaoSeguinte()
+            {
+                Nodo auxNodo = new Nodo();
+                Console.WriteLine("Count:{0}", NodosPosicaoSeguinte.Count);
+                while (NodosPosicaoSeguinte.Count > 0)
+                {
+                    auxNodo = NodosPosicaoSeguinte.Pop();
+                    if (auxNodo.valor == "|")
+                        continue;
+                    calculaPosicaoSeguinte(auxNodo);
+                }
+            }
+
+            private void calculaPosicaoSeguinte(Nodo nodo)
+            {
+                Console.WriteLine("calculaPosicaoSeguinte");
+                switch (nodo.valor)
+                {
+                    case "*":
+                        //i é uma posição em ultima_pos(n), então todas  as posicoes em primeira_pos(n) estão em pos_seguinte(i)
+                        incluiPosicoesNosIndicadores(nodo.primeira_posicao, nodo.ultima_posicao);
+                        break;
+                    case ".":
+                        //i é uma posição em ultima_pos(c1), então todas as posições em primeira_pos(c2) estão em pos_seguinte(i)
+                        incluiPosicoesNosIndicadores(nodo.nodoDireita.primeira_posicao, nodo.nodoEsquerda.ultima_posicao);
+                        break;
+                }
+            }
+
+            private void incluiPosicoesNosIndicadores(HashSet<int> posicoes, HashSet<int> indicadores)
+            {
+                foreach(var i in indicadores)
+                {
+                    if (!posicao_seguinte.ContainsKey(i))
+                    {
+                        posicao_seguinte.Add(i, posicoes);
+                    }
+                    else
+                    {
+                        HashSet<int> indiUpdated = (HashSet<int>)posicao_seguinte[i];
+                        indiUpdated.UnionWith(posicoes);
+                        posicao_seguinte[i] = indiUpdated;
+                    }
+                        
+                }
             }
         }
     }
