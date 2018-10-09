0a72c7e9ec79085d961aacd7fb635f5d1640105b
LENOVO <LENOVO@ACER-PC>
2018-10-09 16:43:36 -0300

    v1.0

--- Trabalho1/Trabalho1/NovoAutomato.cs
+++ Trabalho1/Trabalho1/NovoAutomato.cs
@@ -12,12 +12,12 @@ namespace Trabalho1 {
 			this.Build ();
 		}
 
-		protected void OnButtonOkClicked (object sender, EventArgs e) {
-			string[] vetorEstados = entry2.Text.Split(' ');
-			string[] vetorSimbolos = entry3.Text.Split (' ');
-			janela.atualizarAutomato (vetorEstados, vetorSimbolos);
-			this.Hide ();
-		}
+		//protected void OnButtonOkClicked (object sender, EventArgs e) {
+		//	string[] vetorEstados = entry2.Text.Split(' ');
+		//	string[] vetorSimbolos = entry3.Text.Split (' ');
+		//	janela.atualizarAutomato (vetorEstados, vetorSimbolos);
+		//	this.Hide ();
+		//}
 
 		protected void OnButtonCancelClicked (object sender, EventArgs e) {
 			this.Hide ();
